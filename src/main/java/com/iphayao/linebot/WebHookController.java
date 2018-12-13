8e2dd11bb512e1adc3608fd1d4282d314a025bd9
Karnawat <endkarn0@gmail.com>
2018-12-13 17:43:42 +0700

    update version

--- src/main/java/com/iphayao/linebot/WebHookController.java
+++ src/main/java/com/iphayao/linebot/WebHookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebHookController {
 
-    @PostMapping("/callback")
+    @PostMapping("/callbackk")
     String callBack(){
         return "call back";
     }
