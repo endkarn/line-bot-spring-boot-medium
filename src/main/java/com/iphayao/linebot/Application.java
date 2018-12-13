31edd0a17289074f6a6fe2242f0bb090cf7fad4c
Karnawat <endkarn0@gmail.com>
2018-12-13 16:05:12 +0700

    first commit

--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -1,18 +1,25 @@
 package com.iphayao.linebot;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @SpringBootApplication
+@LineMessageHandler
 public class Application {
-    static Path downloadedContentDir;
-
-    public static void main(String[] args) throws IOException {
-        downloadedContentDir = Files.createTempDirectory("line-bot");
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @EventMapping
+    public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+        System.out.println("event: " + e);
+        TextMessageContent message = e.getMessage();
+        return new TextMessage(message.getText());
     }
 }
