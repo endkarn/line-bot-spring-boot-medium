48360ed5f28f85904aa5c4715b7c1305f0c4ee36
Karnawat <endkarn0@gmail.com>
2018-12-18 16:29:57 +0700

    revent to original version

--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -1,31 +1,18 @@
 package com.iphayao.linebot;
 
-import com.linecorp.bot.model.message.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
-@LineMessageHandler
 public class Application {
-    public static void main(String[] args) {
+    static Path downloadedContentDir;
+
+    public static void main(String[] args) throws IOException {
+        downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(Application.class, args);
-    }
-
-    @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
-    }
-
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
     }
 }
--- src/main/java/com/iphayao/linebot/LineBotConfigure.java
+++ src/main/java/com/iphayao/linebot/LineBotConfigure.java
@@ -1,18 +1,23 @@
-//package com.iphayao.linebot;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Slf4j
-//@Configuration
-//public class LineBotConfigure implements WebMvcConfigurer {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        String downloadedContentUri = Application.downloadedContentDir.toUri().toASCIIString();
-//        log.info("downloaded Uri: {}", downloadedContentUri);
-//        registry.addResourceHandler("/downloaded/**")
-//                .addResourceLocations(downloadedContentUri);
-//    }
-//}
+package com.iphayao.linebot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class LineBotConfigure implements WebMvcConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(LineBotConfigure.class);
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String downloadedContentUri = Application.downloadedContentDir.toUri().toASCIIString();
+        log.info("downloaded Uri: {}", downloadedContentUri);
+        registry.addResourceHandler("/downloaded/**")
+                .addResourceLocations(downloadedContentUri);
+    }
+}
