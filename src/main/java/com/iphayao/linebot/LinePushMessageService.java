84009a51d1065cf76a314874ebb1e594fa1b7c79
Karnawat <endkarn0@gmail.com>
2018-12-18 17:07:34 +0700

    update some broadcast message

--- src/main/java/com/iphayao/linebot/LinePushMessageService.java
+++ src/main/java/com/iphayao/linebot/LinePushMessageService.java
@@ -7,6 +7,7 @@ import com.linecorp.bot.model.response.BotApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 public class LinePushMessageService {
@@ -18,7 +19,7 @@ public class LinePushMessageService {
                 .builder("dK2vVVNMwfM9LpESDP5ZO+NSdG95rR7/ZMDMboZykGrz7PfIc36wurdMV5hruEg43LLlThWM4tZdT4cUQ03SSTjivHDNbQabySkuOe+G0bNrTboNIQ5ZgllcQ1KIlnTYcUOQwIkgYLBIXQE45r9+3gdB04t89/1O/w1cDnyilFU=")
                 .build();
 
-        final TextMessage textMessage = new TextMessage("Heroku Status : Online");
+        final TextMessage textMessage = new TextMessage("Heroku Status : Online ["+new Date()+"]");
         final PushMessage pushMessage = new PushMessage(
                 "U79c1a767e9d7db6466d826af886103dd",
                 textMessage);
