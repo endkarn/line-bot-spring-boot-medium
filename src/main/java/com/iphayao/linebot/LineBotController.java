71d62904f484c53811e8980fee229d97044c40ec
Karnawat <endkarn0@gmail.com>
2018-12-19 14:50:58 +0700

    check pathRichMenu

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -141,8 +141,8 @@ public class LineBotController {
             case "flex": {
 //                String pathImageFlex = new ClassPathResource("richmenu/richmenu-flexs.jpg").getFile().getPath();
 //                String pathConfigFlex = new ClassPathResource("richmenu/richmenu-flexs.yml").getFile().getPath();
-                String pathImageFlex = "richmenu/richmenu-flexs.jpg";
-                String pathConfigFlex = "richmenu/richmenu-flexs.yml";
+                String pathImageFlex = "/richmenu/richmenu-flexs.jpg";
+                String pathConfigFlex = "/richmenu/richmenu-flexs.yml";
                 String userId = event.getSource().getUserId();
                 if (userId != null) {
                     lineMessagingClient.getProfile(userId)
