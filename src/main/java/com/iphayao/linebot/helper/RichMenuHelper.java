72e93b10d3fac019c0f059999a1c29b435f11544
Karnawat <endkarn0@gmail.com>
2018-12-19 15:30:04 +0700

    check pathRichMenu

--- src/main/java/com/iphayao/linebot/helper/RichMenuHelper.java
+++ src/main/java/com/iphayao/linebot/helper/RichMenuHelper.java
@@ -110,7 +110,7 @@ public class RichMenuHelper {
                 .configure(INDENT_OUTPUT, true);
 
         Object yamlAsObject;
-        try(FileInputStream is = new FileInputStream(path)) {
+        try(FileInputStream is = new FileInputStream("richmenu/richmenu-flexs.yml")) {
             yamlAsObject = YAML.load(is);
         }
 
